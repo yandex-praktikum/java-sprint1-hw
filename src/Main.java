50b8ad082e631c0055cac44a64c2188d434db74e
Aleksey Oshurkov <luckysmo@yandex.ru>
2022-02-11 21:30:38 +0400

    refactor: Класс TaskManager перенесен в пакет манагер

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
-import Tasks.Epic;
-import Tasks.SubTask;
-import Tasks.Task;
+import manager.TaskManager;
+import tasks.Epic;
+import tasks.SubTask;
+import tasks.Task;
 
 public class Main {
     public static void main(String[] args) {
--- src/tasks/SubTask.java
+++ src/tasks/SubTask.java
@@ -1,4 +1,4 @@
-package Tasks;
+package tasks;
 
 public class SubTask extends Task {
 
